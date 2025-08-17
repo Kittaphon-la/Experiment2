6abcdb8c339716d722ff9db9f64052a0936b1c17
Kittaphon-la <kittaphon_la67@live.rmutl.ac.th>
2025-08-18 00:30:45 +0700

    Set base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Experiment2/'  // 🔹 ชื่อ repo ของคุณ
+  base: '/Experiment2/'  // 🔹 ต้องตรงกับชื่อ repo ของคุณ
 })
